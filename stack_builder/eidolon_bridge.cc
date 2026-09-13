c675d39605494790b623442602fe1e51160ffc0d
worker9 <notexistedemail@mail.ru>
2026-09-13 18:32:15 +0200

    auto build chromium (153.0 8010.36)

--- stack_builder/eidolon_bridge.cc
+++ stack_builder/eidolon_bridge.cc
@@ -1125,11 +1125,11 @@ EIDOLON_EXPORT EidolonHandle eidolon_listen_quic(const char* host, uint16_t port
 
 } // extern "C"
 
-#if BUILDFLAG(IS_ANDROID)
+#if defined(__ANDROID__) || defined(OS_ANDROID) || defined(ANDROID)
 #include <jni.h>
 #include "base/android/jni_android.h"
 
-extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+extern "C" __attribute__((visibility("default"))) jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     base::android::InitVM(vm);
     return JNI_VERSION_1_4;
 }
