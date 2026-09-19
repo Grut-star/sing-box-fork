f787d9408ea9e6b8eddba1b46f1be8d467e6b9e2
worker9 <notexistedemail@mail.ru>
2026-09-19 10:18:05 +0200

    auto build chromium (153.0 8010.36)

--- stack_builder/eidolon_bridge.cc
+++ stack_builder/eidolon_bridge.cc
@@ -485,6 +485,7 @@ private:
     void Run() {
         net::URLRequestContextBuilder builder;
         builder.DisableHttpCache();
+        builder.DisableNetworkChangeNotifier();
 
         builder.set_proxy_config_service(
                 std::make_unique<net::ProxyConfigServiceFixed>(
@@ -624,6 +625,7 @@ private:
         // 1. Делегируем сборку всего стека (включая TransportSecurityState) билдеру
         net::URLRequestContextBuilder builder;
         builder.DisableHttpCache();
+        builder.DisableNetworkChangeNotifier();
 
         // Явно отключаем поиск системных прокси (PAC/WPAD)
         // Явно отключаем прокси через фиксированный конфиг, чтобы не падали DCHECK при ошибках SSL
